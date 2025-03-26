42f5fbfece479fc6e674381147903edcfff12381
jalvlue <1722292195@qq.com>
2025-03-26 19:44:29 +0800

    check1: raw ip datagram

--- apps/ip_raw.cc
+++ apps/ip_raw.cc
@@ -1,4 +1,5 @@
 #include "socket.hh"
+#include <iostream>
 
 using namespace std;
 
@@ -8,9 +9,49 @@ public:
   RawSocket() : DatagramSocket( AF_INET, SOCK_RAW, IPPROTO_RAW ) {}
 };
 
+auto zeroes( auto n )
+{
+  return string( n, 0 );
+}
+
+void send_internet_datagram( const string& payload )
+{
+  RawSocket sock;
+  string datagram;
+  datagram += char( 0b0100'0101 ); // v4, and headerlength 5words
+  datagram += zeroes( 7 );
+  datagram += char( 64 );  // TTL
+  datagram += char( 5 );   // protocol
+  datagram += zeroes( 6 ); // checksum + src address
+  datagram += char( 34 );
+  datagram += char( 93 );
+  datagram += char( 94 );
+  datagram += char( 131 );
+  datagram += payload;
+  sock.sendto( Address { "1" }, datagram );
+}
+
+void send_icmp_message( const string& payload )
+{
+  send_internet_datagram( "\x08" + payload );
+}
+
+void program_body()
+{
+  string payload;
+  while ( cin.good() ) {
+    getline( cin, payload );
+    send_icmp_message( payload + "\n" );
+  }
+}
+
 int main()
 {
-  // construct an Internet or user datagram here, and send using the RawSocket as in the Jan. 10 lecture
-
-  return 0;
+  try {
+    program_body();
+  } catch ( const exception& e ) {
+    cerr << e.what() << "\n";
+    return EXIT_FAILURE;
+  }
+  return EXIT_SUCCESS;
 }
