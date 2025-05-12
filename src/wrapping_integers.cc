d72cb73ea3001b2ba23a4c02f86ee82e4dbad730
jalvlue <1722292195@qq.com>
2025-05-12 19:12:05 +0800

    check2: wrap ok

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -1,15 +1,17 @@
 #include "wrapping_integers.hh"
 #include "debug.hh"
+#include <cstdint>
 
 using namespace std;
 
+// absolute seqno -> seqno
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
   // Your code here.
-  debug( "unimplemented wrap( {}, {} ) called", n, zero_point.raw_value_ );
-  return Wrap32 { 0 };
+  return Wrap32 { static_cast<uint32_t>( n ) + zero_point.raw_value_ };
 }
 
+// seqno -> absolute seqno
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
   // Your code here.
