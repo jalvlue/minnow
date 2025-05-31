e62a1a2e39c9203f6f0091397e3a7f0bd7fe7830
jalvlue <1722292195@qq.com>
2025-06-01 01:19:46 +0800

    tmp

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -8,13 +8,27 @@ using namespace std;
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
   // Your code here.
-  return Wrap32 { static_cast<uint32_t>( n ) + zero_point.raw_value_ };
+  return zero_point + n;
 }
 
 // seqno -> absolute seqno
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
   // Your code here.
-  debug( "unimplemented unwrap( {}, {} ) called", zero_point.raw_value_, checkpoint );
-  return {};
+  const uint32_t k = this->raw_value_ - zero_point.raw_value_;
+  const uint64_t k64 = k, interval = 1ULL << 32;
+  const int64_t m = ( static_cast<int64_t>( checkpoint ) - static_cast<int64_t>( k64 ) ) / interval;
+  array<int64_t, 3> candidates { m - 1, m, m + 1 };
+  uint64_t best_n = 0, min_distance = UINT64_MAX;
+
+  for ( const auto& candidate_m : candidates ) {
+    const uint64_t candidate_n = k64 + static_cast<uint64_t>( candidate_m ) * interval;
+    const uint64_t distance
+      = ( candidate_n > checkpoint ) ? ( candidate_n - checkpoint ) : ( checkpoint - candidate_n );
+    if ( distance < min_distance || ( distance == min_distance && candidate_n < best_n ) ) {
+      min_distance = distance;
+      best_n = candidate_n;
+    }
+  }
+  return best_n;
 }
